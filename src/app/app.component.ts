78b77f2b4b69cfbcaf3aa1a7e9a79a954f1aff17
BanyMaciej <banymaciej@gmail.com>
2019-06-10 19:28:24 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { SoundProcessorService } from './services/sound-processor.service';
 import { SoundAnalyzerService } from './services/sound-analyzer.service';
-import { NotesRecognizerService } from './services/notes-recognizer.service';
 import * as _ from 'underscore';
 
 @Component({
@@ -19,7 +18,7 @@ export class AppComponent implements OnInit {
 
   constructor(private soundProcessor: SoundProcessorService,
               private soundAnalyzer: SoundAnalyzerService,
-              private notesRecognizer: NotesRecognizerService) {}
+              ) {}
 
   ngOnInit() {}
   start() {
